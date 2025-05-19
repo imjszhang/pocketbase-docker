e4f0d0c5da0e0cd470ee75b0dbff4af32fe2677b
imjszhang <ortle3x3@gmail.com>
2025-05-19 19:21:17 +0800

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 # 如果提供了超级用户凭据，则检查并创建超级用户
 if [ -n "$ADMIN_EMAIL" ] && [ -n "$ADMIN_PASSWORD" ]; then
   # 简化判断逻辑，只检查数据目录和数据库文件是否存在
-  if [ ! -d "/pb_data" ] || [ ! -f "/pb_data/data.db" ]; then
+  if [ ! -d "/usr/local/bin/pb_data" ] || [ ! -f "/usr/local/bin/pb_data/data.db" ]; then
     echo "数据目录或数据库文件不存在，创建超级用户..."
     /usr/local/bin/pocketbase superuser create "$ADMIN_EMAIL" "$ADMIN_PASSWORD"
   else
